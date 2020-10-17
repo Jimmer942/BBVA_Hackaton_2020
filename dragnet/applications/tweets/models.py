adeb97008aaddc33477ecacf8d5cc71900fc3ca4
Jimmer942 <942@holbertonschool.com>
2020-10-17 14:22:56 -0500

    Se define el model para los tweets

--- dragnet/applications/tweets/models.py
+++ dragnet/applications/tweets/models.py
@@ -3,3 +3,23 @@ from django.db import models
 # Create your models here.
 class TweetModel(models.Model):
 
+    text = models.CharField(max_length=250)
+    id = models.IntegerField()
+    usuario = models.CharField(max_length=250)
+    fecha = models.DateField()
+    likes = models.IntegerField()
+    retweets = models.IntegerField()
+    comentarios = models.IntegerField()
+    pais = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.nombre
+
+    def get_full_description(self):
+        return f'nombre {self.nombre}, text {self.text}, 
+        id {self.id}, usuario {self.usuario}, fecha {self.fecha},
+        likes {self.likes}, retweets {self.retweets}, comentarios {self.comentarios},
+        pais {self.pais}'
+
+
+
