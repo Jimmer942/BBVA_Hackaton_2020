6a3cef1d8543577e1c42fdc3b1e33f1c51045002
Jimmer942 <942@holbertonschool.com>
2020-10-17 15:42:29 -0500

    Renombran los atributos

--- dragnet/applications/tweets/models.py
+++ dragnet/applications/tweets/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class TweetModel(models.Model):
 
-    text = models.CharField(name='text', max_length=250)
+    full_text = models.CharField(name='text', max_length=250)
     id = models.IntegerField()
     user = models.CharField(name='usuario', max_length=250)
     created_at = models.DateField(name='fecha_creacion') # fecha de creacion
@@ -16,7 +16,7 @@ class TweetModel(models.Model):
 
     def get_full_description(self):
         return f'user {self.user},' \
-               f' text {self.text}, id {self.id},' \
+               f' full_text {self.full_text}, id {self.id},' \
                f' created_at {self.fecha}, likes {self.likes},' \
                f' retweet_count {self.retweets},' \
                f' geo {self.geo}'
