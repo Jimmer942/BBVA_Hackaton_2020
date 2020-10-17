90b78e28ed1611018f21b6b2d323b0d56329af89
Alb4tr02 <jodmunozol@unal.edu.co>
2020-10-17 14:39:21 -0500

    namesç

--- dragnet/applications/tweets/models.py
+++ dragnet/applications/tweets/models.py
@@ -3,23 +3,25 @@ from django.db import models
 # Create your models here.
 class TweetModel(models.Model):
 
-    text = models.CharField(max_length=250)
+    text = models.CharField(name='text', max_length=250)
     id = models.IntegerField()
-    usuario = models.CharField(max_length=250)
-    fecha = models.DateField()
-    likes = models.IntegerField()
-    retweets = models.IntegerField()
-    comentarios = models.IntegerField()
-    pais = models.CharField(max_length=250)
+    usuario = models.CharField(name='usuario', max_length=250)
+    fecha = models.DateField(name='fecha_creacion')
+    likes = models.IntegerField(name='n_likes')
+    retweets = models.IntegerField(name='n_rt')
+    comentarios = models.IntegerField(name='n_comentarios')
+    pais = models.CharField(name='pais', max_length=250)
 
     def __str__(self):
         return self.nombre
 
     def get_full_description(self):
-        return f'nombre {self.nombre}, text {self.text}, 
-        id {self.id}, usuario {self.usuario}, fecha {self.fecha},
-        likes {self.likes}, retweets {self.retweets}, comentarios {self.comentarios},
-        pais {self.pais}'
+        return f'nombre {self.nombre},' \
+               f' text {self.text}, id {self.id},' \
+               f' usuario {self.usuario},' \
+               f' fecha {self.fecha}, likes {self.likes},' \
+               f' retweets {self.retweets},' \
+               f' comentarios {self.comentarios},pais {self.pais}'
 
 
 
